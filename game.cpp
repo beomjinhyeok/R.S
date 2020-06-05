c8a7599ae25e5bd2073928e675c3cd7a670179a2
unknown <hamacu6@gmail.com>
2020-06-05 17:43:54 +0900

    포커 베팅 업데이트

--- game.cpp
+++ game.cpp
@@ -1,38 +1,54 @@
 #include "poker_header.h"
 
 
-//card givePlayer(player member[])
-//{
-//	card temp1, temp2;
-//	for (int i = 0; i < survivor; i++)
-//	{
-//		temp1 = drawCard();
-//		temp2 = drawCard();
-//		member[i].getCard(temp1, temp2);
-//	}
-//}
-
-void choiceBet(player member[], const int gambler, int leader, int &totalBet)
+card drawCard()
 {
-	int nowBet = leader, betMoney;
-	for (int i = 0; i < gambler; i++, nowBet++)
-	{
-		if (nowBet == gambler)
-			nowBet == 0;
-		nowBet = gambler % nowBet;
+	card temp;
+	temp = drawCard();
+
+	return drawCard();
+}
+
+
+void choiceBet(player member[], int& gambler, int leader, int &totalBet)
+{
+	int nowBet = leader-1, betMoney=0;
+
+	int cantChoice;
+
+	for (int i = 0; i < gambler; i++)
+	{	
+		nowBet = gambler % (nowBet + 1);
+		
+		while (member[nowBet].nowPlay() == false) //다음 플레이어가 이번판 다이를 쳤을 경우.
+		{
+			nowBet = gambler % (nowBet + 1);
+		}
 
 		if (i == 0) //첫 턴 leader만 배팅 다름.
 		{
-			betMoney = member[nowBet].inputBet(totalBet);
+			betMoney = member[nowBet].leaderBet(totalBet,gambler);
+			while (betMoney == 0) //만약 리더가 다이한 경우
+			{
+				if (gambler==1) // 한 명빼고 gambler가 없을 경우
+				{
+					break;
+				}
+				nowBet = gambler % (nowBet + 1);
+				betMoney = member[nowBet].leaderBet(totalBet,gambler);
+			}
 		}
 		else
 		{
-			choiceBet(member[nowBet],betMoney,totalBet);
+			if (member[nowBet].canBet == true) // 돈 낼 능력이 있는가 판단
+				choiceFollower(member[nowBet], betMoney, totalBet, gambler, i);
+			else
+				cant_Bet(member[nowBet], betMoney, gambler,totalBet);
 		}
 	}
 }
 
-void choiceBet(player& follwer,int& betMoney,int& totalBet)
+void choiceFollower(player& follower,int& betMoney,int& totalBet,int& gambler, int &bet)
 {
 	int choice;
 	cin >> choice; // 인터페이스에서는 선택
@@ -40,26 +56,53 @@ void choiceBet(player& follwer,int& betMoney,int& totalBet)
 	switch (choice)
 	{
 	case 1: //콜
-		follwer.payMoney(betMoney,totalBet);
+		follower.payMoney(betMoney,totalBet);
 		break;
-	case 2: //
+	case 2: //다이
+		follower.playerDie(gambler);
 		break;
-	case 3:
+	case 3: // 더블, 배팅 금액 없는 경우 생각
+		bet = 0; // 더블 다음 사람부터 다시 팔로워처럼 배팅 선택
+		follower.doubleBet(betMoney,totalBet);
 		break;
-	case 4:
-
+	case 4: // 하프
+		if (betMoney > totalBet / 2)
+		{
+			cout << "하프가 배팅 금액보다 적습니다.\n";
+			choiceFollower(follower, betMoney, totalBet, gambler, bet);
+		}
+		else if (follower.getMoney < totalBet/2)
+		{
+			cout << "현재 가진 돈이 판돈의 절반이 되지 않습니다.\n";
+			choiceFollower(follower, betMoney, totalBet, gambler, bet);
+		}
+		else
+		{
+			follower.halfBet(betMoney, totalBet);
+			bet = 0;
+		}
+		break;
+	case 5: //All in
+		bet = 0; // 올인 다음 사람부터 다시 팔로워처럼 배팅 선택
+		follower.allMoney(betMoney,totalBet);
 	}
 }
 
-
-void giveCard(int survive,int leader)
+void cant_Bet(player& follwer, int& betMoney,int &gambler,int & totalBet)
 {
-	leader += 1;
-	for (int i = 0; i < survive; i++)
-	{
+	int choice;
+	cin >> choice;
 
+	switch (choice)
+	{
+	case 1: // 올인 
+		follwer.dieAllMoney(totalBet);
+		break;
+	case 2: // 다이
+		follwer.playerDie(gambler);
 	}
 }
+
 
 card drawCard()
 {
@@ -69,7 +112,7 @@ card drawCard()
 
 void obliBet(player member[],int &totalBet) // 의무 배팅
 {
-	int obli = 100000000;
+	int obli = 3;
 
 	for (int i = 0; i < survivor; i++)
 	{
@@ -86,6 +129,7 @@ void openCard(player open) // 인터페이스 요소 생각중
 	cout << open.myCard[1];
 }
 
+
 int choiceLeader(player member[],const int gambler) // 손봐야함
 {
 	int temp = member[0].getMoney(), leader=0;
@@ -98,7 +142,6 @@ int choiceLeader(player member[],const int gambler) // 손봐야함
 			leader = i;
 		}
 	}
-
 	return leader;
 }
 
@@ -116,14 +159,18 @@ void playGame(player member[],player& dealer) // 게임 진행
 		member[i].takeCard;
 	}
 
-	openCard(member[0]); // 삭제 고려
+	for(int i =0; i<3; i++)
+		dealer.takeCard;
+	
+	leader = choiceLeader(member, gambler); // 리더는 한 번만 정하면 다음부터는 오른쪽 사람이 리더
 
-	leader = choiceLeader(member,gambler);
-
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < 2; i++)
 	{
-
+		
 	}
+
+
 }
 
-//진행 상황 - 의무 배팅 구현, 카드 주기 구현.
+//진행 상황 - 의무 배팅 구현, 카드 2장 주기 구현, 딜러 카드 3장 구현, 리더 선택 구현, 리더 배팅 구현, 팔로워 배팅 구현(예외사항 적용중)
+
--- playerClass.cpp
+++ playerClass.cpp
@@ -6,13 +6,13 @@ void player::showMoney() // 남은 플레이어 돈 확인
 	cout << gameMoney << endl;
 }
 
-void player::payMoney(int pay, int& totalBet) //첫 의무돈 지불
+void player::payMoney(int pay, int& totalBet) //첫 의무돈 지불, 콜
 {
 	if (pay >= gameMoney)
 	{
 		gameMoney = 0;
 		totalBet = totalBet + gameMoney;
-		// 이제부터 스킵하는 함수.
+		play = false;
 	}
 
 	else
@@ -22,9 +22,11 @@ void player::payMoney(int pay, int& totalBet) //첫 의무돈 지불
 	}
 }
 
-void player::playerDie() // 플레이어 컴퓨터 구분;
+void player::playerDie(int& gambler) 
 {
 	play = false;
+	die = true;
+	gambler--;
 }
 
 void player::getCard(card newCard1, card newCard2)
@@ -39,11 +41,6 @@ bool player::checkPlayer()
 		return true;
 	else
 		return false;
-}
-
-int player::getMyNumber()
-{
-	return myNumber;
 }
 
 void player::takeCard()
@@ -62,10 +59,68 @@ int player::inputBet(int &totalBet) // 베팅 범위 정해야 함.
 	do 
 	{
 		cin >> betMoney;
-	} while (betMoney>gameMoney || betMoney < 0);
+	} while (betMoney>gameMoney || betMoney <= 0);
 
 	totalBet = totalBet + betMoney;
 	gameMoney = gameMoney - betMoney;
 
 	return betMoney;
+}
+
+int player::leaderBet(int &totalMoney,int& gambler)
+{
+	int choice, betMoney;
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 1:
+		betMoney = inputBet(totalMoney);
+		return betMoney;
+	case 2:
+		playerDie(gambler);
+		return 0;
+	}
+}
+
+void player::doubleBet(int& betMoney,int& totalBet)
+{
+	betMoney = betMoney * 2;
+	gameMoney = gameMoney - betMoney;
+	totalBet = totalBet + betMoney;
+}
+
+void player::allMoney(int& betMoney, int& totalBet)
+{
+	betMoney = gameMoney;
+	gameMoney = 0;
+	totalBet = totalBet + betMoney;
+	play = false;
+}
+
+bool player::canBet(int betMoney) //배팅할 능력이 있는지 판단
+{
+	if (betMoney > gameMoney)
+		return false;
+	else
+		return true;
+}
+
+void player::halfBet(int& betMoney, int& totalBet)
+{
+	betMoney = totalBet / 2;
+	gameMoney = gameMoney - betMoney;
+	totalBet = betMoney;
+}
+
+void player::dieAllMoney(int& totalBet)
+{
+	totalBet = totalBet + gameMoney;
+	gameMoney = 0;
+	play = false;
+}
+
+bool player::nowPlay()
+{
+	return play;
 }
